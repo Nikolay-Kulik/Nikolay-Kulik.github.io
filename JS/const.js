6fe728295dc092ead2376a623616fe74be974f68
Nikolay-Kulik <113473437+Nikolay-Kulik@users.noreply.github.com>
2023-01-09 18:07:09 +0200

    Create const.js

---
+++ JS/const.js
@@ -0,0 +1,21 @@
+// добавление переменных
+const one = document.querySelector('.one')
+const oneList = document.querySelector('.one__list')
+const oneLi = document.querySelectorAll('.one__list li')
+const oneButton = document.querySelector('.one__button')
+const input = document.querySelectorAll('.one__list li input')
+
+
+const two = document.querySelector('.two')
+const twoList = document.querySelector('.two__list')
+const twoLi = document.querySelectorAll('.two__list li')
+const twoButton = document.querySelector('.two__button')
+const output = document.querySelectorAll('.output')
+
+const number = document.querySelectorAll('.number')
+const arrowStep = document.querySelectorAll('.arrowStep')
+const arrowBack = document.querySelectorAll('.arrowBack')
+const hint = document.querySelector('.hint')
+const keyNum1 = document.querySelector('.keyNum1')
+const keyNum2 = document.querySelector('.keyNum2')
+
